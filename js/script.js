69c67ca4e21218349973d17ffaeaab7e1dc303b1
Bogdan <bogdannalapko2@gmail.com>
2020-05-14 12:43:05 +0200

    2 punkt 12 lection

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 "use strict";
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
     
