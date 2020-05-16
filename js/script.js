e1bd19ca1e517a65cca62ae1733a1f0c93de6a52
Bogdan <bogdannalapko2@gmail.com>
2020-05-17 00:06:26 +0200

    finish lecture 15

--- js/script.js
+++ js/script.js
@@ -1,26 +1,52 @@
+/* * Задание на урок:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+ 
+
 "use strict";
 
-let num = 50;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-/* while (num <= 55) {
-    console.log(num);
-    num++;
-} */
-/* 
-do {
-    console.log(num);
-    num++;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDb.movies[a] = b;
+        console.log('done');  
+    } else {
+        console.log('error');
+        i--;
+    }  
 }
-while (num < 55); */
 
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-         continue;
-        /*  break; */
-    }
-
-
-    console.log(i);
-   
+if (personalMovieDb.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDb.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
 }
+
+console.log(personalMovieDb);
 
